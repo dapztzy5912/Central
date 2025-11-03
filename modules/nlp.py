84f7ec5878826e3ad732456bc2bdb9a0056c4154
dapztzy5912 <depannation999@gmail.com>
2025-11-03 21:01:41 +0700

    Create nlp.py

---
+++ modules/nlp.py
@@ -0,0 +1,6 @@
+# modules/nlp.py
+def analyze(text):
+    # Di sini lo bisa pake library NLP kayak NLTK atau spaCy
+    # Buat tokenisasi, stemming, atau analisis sentimen
+    # Contoh sederhana:
+    return text.lower()
